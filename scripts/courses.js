55c730f735806dd1f307a0e1ba3f91470438529d
fredricagerling <f.agerling@hotmail.com>
2021-03-31 15:59:35 +0200

    refactored JS-file

--- scripts/courses.js
+++ scripts/courses.js
@@ -5,32 +5,31 @@ const displayAmountOfCourses = document.querySelector('#info-text');
 const categories = document.querySelector('#courseCategories');
 const filterButtons = document.querySelector('.filter-button');
 
-const showAllCourses = document.querySelector('#showAllCourses').addEventListener('click', () => {
+const allCoursesButton = document.querySelector('#showAllCourses').addEventListener('click', () => {
     loadCoarses().then(data => createCourseContainer(data)).catch(err => console.log(err));
-    
-    courseCategory = 'Visa alla';
+
+    selectedCategory = 'Visa alla';
 });
 
 const bestSellersButton = document.querySelector('#bestSellers').addEventListener('click', () => {
     bestsellerCourses().then(data => createCourseContainer(data)).catch(err => console.log(err));
 
-    courseCategory = 'Bästsäljare';
+    selectedCategory = 'Bästsäljare';
 });
 
-let courseCategory;
-let courseCategories = [];
+let selectedCategory = 'Visa alla';
 
 function updateTextParagraph(count) {
     let text;
 
-    switch (courseCategory) {
+    switch (selectedCategory) {
         case 'Bästsäljare':
             text = 'Det här är våra tre högst rankade kurser';
             break;
         case 'Visa alla':
             text = `Vi har ${count} stycken video on demand kurser i vårat bibliotek`;
             break;
-        default: text = `Vi har ${count} stycken video on demand kurser inom ${courseCategory}`;
+        default: text = `Vi har ${count} stycken video on demand kurser inom ${selectedCategory}`;
     }
     displayAmountOfCourses.innerHTML = text;
 }
@@ -56,23 +55,26 @@ function displayCourse(course) {
             <p>${course.description}</p>
         </div>`);
 
-    if (!courseCategories.includes(course.courseType)) {
-        courseCategories.push(course.courseType);
-        createFilterButton(course.courseType);
-    }
+    createFilterButton(course.courseType);
 }
 
-function createFilterButton(course) {
-    let button = document.createElement('button');
-    
-    button.textContent = course;
-    button.classList.add('filter-button');
-    categories.appendChild(button);
+function createFilterButton(courseType) {
+    let element = document.getElementById(`${courseType}Button`);
 
-    button.addEventListener('click', () => {
-        filterCourses(course).then(data => createCourseContainer(data)).catch(err => console.log(err));
-        courseCategory = course;
-    });
+    if (element == null) {
+
+        let button = document.createElement('button');
+
+        button.textContent = courseType;
+        button.classList.add('filter-button');
+        button.id = `${courseType}Button`;
+        categories.appendChild(button);
+
+        button.addEventListener('click', () => {
+            filterCourses(courseType).then(data => createCourseContainer(data)).catch(err => console.log(err));
+            selectedCategory = courseType;
+        });
+    }
 }
 
 async function bestsellerCourses() {
